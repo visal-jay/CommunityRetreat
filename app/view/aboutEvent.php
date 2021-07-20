a4b8bab7dc6748a73de9768c1d1cb269949f1148
VenodiW <72806077+VenodiW@users.noreply.github.com>
2021-07-20 09:04:03 +0530

    Venodi 1.0

---
+++ app/view/aboutEvent.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Public/assets/newstyles.css">
+    <script src="https://kit.fontawesome.com/c119b7fc61.js" crossorigin="anonymous"></script>
+    <title>About Event</title>
+</head>
+
+<style>
+    .container-size{
+        width: 70%;
+    }
+</style>
+
+<?php include "nav.php" ?>
+
+<body>
+    <div class="flex-col flex-center margin-side-lg">
+        <h1>About</h1>
+        <div class="nav-secondary">
+            <div class="nav-secondary-bar margin-lg">
+                <a class="btn margin-side-md" style=" margin-bottom:10px;" href="# ">Volunteer</a>
+                <a class="btn margin-side-md" style=" margin-bottom:10px;" href="# ">Donate</a>
+            </div>
+        </div>
+
+        <div class="content border-round container-size" style= "background-color: #eeeeee">
+            <p class= "margin-lg">In this area description about a specific event will be displayed as a paragraph....</p>
+            <a class="btn margin-side-md" style="margin-bottom:10px;" href="# ">Publish</a>
+        </div>
+    </div>
+</body>
+
+<script></script>
+
+</html>

ada5706d78e49f6584e1c933ed5cb903c6b7a787
Manuka Dewanarayana <84101425+manudew@users.noreply.github.com>
2021-08-24 18:04:33 +0530

    change

--- app/Model/RegisteredUser.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class RegisteredUser extends User
-{
-
-}
--- app/view/employees.php
+++
@@ -1,6 +0,0 @@
-
-<?php
-foreach ($employees as $person) {
-    echo $person["name"] . " | " . $person["nic"]. " | " . $person["dob"] . "<br>";
-}
-?>

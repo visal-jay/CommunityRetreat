f94b960a2ca1eee6b305a339ff2402ff754ceacd
VenodiW <72806077+VenodiW@users.noreply.github.com>
2021-09-07 01:56:17 +0530

    tab

--- app/view/aboutEvent.php
+++ app/view/aboutEvent.php
@@ -356,8 +356,7 @@ if (isset($_SESSION["user"]["user_type"])) {
             <?php if ($volunteer_status == 1) { ?>
                 <div class="flex-col flex-center content border-round container-size1 margin-md" style="background-color: #03142d">
                     <p class="margin-md" style="color:white; text-align:center">Interested in joining hands with us?</p>
-                    <div class="progress" data-width="<?php if ($volunteer_percent == NULL) echo "0";
-                                                        else $volunteer_percent ?>%">
+                    <div class="progress" data-width="<?php if ($volunteer_percent == NULL) echo "0"; else $volunteer_percent ?>%">
                         <div class="volunteers-progress-bar"></div>
                     </div>
                     <button class="btn clr-green margin-md"><i class="fas fa-user-friends"></i>&nbsp;I want to
@@ -369,8 +368,7 @@ if (isset($_SESSION["user"]["user_type"])) {
                 <div class="flex-col flex-center content border-round container-size1 margin-md" style="background-color: #03142d; text-align:center">
                     <p style="color:white">Would you like to give value to your hard-earned money by contributing to this
                         community service project?</p>
-                    <div class="progress" data-width="<?php if ($dotaion_percent == NULL) echo "0";
-                                                        else $dotaion_percent ?>%">
+                    <div class="progress" data-width="<?php if ($dotaion_percent == NULL) echo "0"; else $dotaion_percent ?>%">
                         <div class="donaters-progress-bar"></div>
                     </div>
                     <button class="btn clr-green margin-md" onclick="togglePopup('form'); blur_background('background');stillBackground('id1')"><i class="fas fa-hand-holding-usd"></i>&nbsp;Donate Now!</button>

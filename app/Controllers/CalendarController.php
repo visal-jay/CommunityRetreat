b649962a08b21732874390b4af9d9f21f94b0265
Manuka Dewanarayana <2019cs031@stu.ucsc.lk>
2021-09-19 14:09:52 +0530

    calendar change 1.0

--- app/Controllers/CalendarController.php
+++ app/Controllers/CalendarController.php
@@ -10,11 +10,7 @@
         public function  getCalendarDetails(){
 
             $calendar = new Calendar();
-            $_SESSION["user"]["uid"]='REG0000032';
             $uid=$_SESSION["user"]["uid"];
-            if(!isset($_SESSION)){
-                session_start();
-            }
             $calendar_details = $calendar->getCalendarDetails($uid);
             echo json_encode($calendar_details);
         }

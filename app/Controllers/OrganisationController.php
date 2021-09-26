cc8b2559dca6148ca00477865e2e4c9e2115db2b
Visal <visaljayathilaka@gmail.com>
2021-09-26 17:21:20 +0530

    input-error

--- app/Controllers/OrganisationController.php
+++ app/Controllers/OrganisationController.php
@@ -49,9 +49,6 @@ class OrganisationController
     public function update()
     {
         Controller::validateForm(["about_us", "longitude","latitude"]);
- /*        var_dump($_POST);
-        var_dump($_GET);
-        exit(); */
         Controller::accessCheck(["organization"]);
         (new Organisation)->updateDetails($_SESSION["user"]["uid"], $_POST);
         Controller::redirect("/Organisation/dashboard");

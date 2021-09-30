3dc8384123021f813fb2b1e41ed2e6b4a1343d0d
VenodiW <72806077+VenodiW@users.noreply.github.com>
2021-10-01 05:17:22 +0530

    chat icon

--- app/view/aboutEvent.php
+++ app/view/aboutEvent.php
@@ -172,7 +172,15 @@
         border-radius: 8px;
     }
 
-
+    .home-events {
+        border-radius: 8px;
+        min-height: max-content;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
+        padding: 2rem 1rem;
+        margin: 1rem 0 2rem 0;
+    }
 
 
     @media screen and (max-width:800px) {
@@ -226,7 +234,14 @@
             flex-direction: column;
         }
 
+        .home-events {
+            grid-template-columns: 1fr;
+            grid-gap: 1rem;
+        }
 
+        .grid-row-1 {
+            grid-row: 1;
+        }
     }
 </style>
 
@@ -242,7 +257,7 @@
                     <div class="date-container">
                         <div class="flex-row margin-lg">
                             <i class="btn-icon icon-width far fa-calendar-alt clr-green margin-side-lg"></i>
-                            <h4 class="head-margin data">Starts on  <?= $start_date ?><br>Ends on  <?= $end_date ?></h4>
+                            <h4 class="head-margin data">Starts on <?= $start_date ?><br>Ends on <?= $end_date ?></h4>
                             <?php if ($organization || $moderator) { ?>
                                 <div class="flex-row-to-col flex-center">
                                     <div class="flex-col">
@@ -261,7 +276,7 @@
                     <div class="time-container">
                         <div class="flex-row margin-lg">
                             <i class="btn-icon icon-width far fa-clock clr-green margin-side-lg"></i>
-                            <h4 class="head-margin data">At  <?= $start_time ?></h4>
+                            <h4 class="head-margin data">At <?= $start_time ?></h4>
                             <?php if ($organization || $moderator) { ?>
                                 <div class="flex-row-to-col flex-center">
                                     <div class="flex-row">
@@ -280,7 +295,7 @@
                     <div class="time-container">
                         <div class="flex-row margin-lg">
                             <i class="btn-icon icon-width fas fa-hourglass-start clr-green margin-side-lg"></i>
-                            <h4 class="head-margin">Duration  <?= $duration ?></h4>
+                            <h4 class="head-margin">Duration <?= $duration ?></h4>
                         </div>
                     </div>
 
@@ -397,9 +412,28 @@
 
 
             </div>
-            <div class="flex-col flex-center content border-round container-size1 margin-md" style="text-align:center">
+
+            <!-- <div class="flex-col flex-center content border-round container-size1 margin-md" style="text-align:center">
                 <img src="/Public/assets/chat.gif" alt="">
                 <button class="btn btn-solid margin-md" onclick="window.location.href='/RegisteredUser/chatApp?new_chat_id=<?= 'EVN' . $_GET['event_id'] ?>'">Chat with us</button>
+            </div> -->
+
+            <div class="flex-col flex-center content border-round container-size1 home-events">
+                <div class="flex-row flex-center">
+                    <img src="/Public/assets/chat.gif" style="width:500px" alt="">
+                </div>
+
+                <div class="flex-col flex-center grid-row-1" style="text-align:center">
+                    <!-- <h1>Be safe and spread <br>Not COVID</h1> -->
+                    <h4 class="margin-md felx-row flex-center">Want to clear out all your doubts?<br>Curious to know who we are?</h4>
+                    <div>
+                        <img src="/Public/assets/doubt-banner.png" style="width:200px" alt="">
+                    </div>
+                    <p>We are just one click away!</p>
+                    <div>
+                        <button class="btn btn-solid margin-md" onclick="window.location.href='/RegisteredUser/chatApp?new_chat_id=<?= 'EVN' . $_GET['event_id'] ?>'">Chat with us</button>
+                    </div>
+                </div>
             </div>
 
             <?php if ($moderator || $organization) { ?>
@@ -431,23 +465,30 @@
             <form action="/Volunteer/volunteerEvent?event_id=<?= $_GET["event_id"] ?>" method="post">
                 <button type="button" class="btn-icon btn-close" onclick="togglePopup('volunteer-form'); blur_background('background'); stillBackground('id1')"><i class="fas fa-times"></i></button>
                 <?php
-                    $event_days = [];
+                $event_days = [];
 
-                    $startDate = new DateTime($start_date);
-                    $interval = new DateInterval('P1D');
-                    $realEnd = new DateTime($end_date);
-                    $realEnd->add($interval);
-                  
-                       
-                    $period = new DatePeriod( $startDate , $interval, $realEnd);
-                    foreach($period as $date) {                 
-                        $event_days = $date->format('Y-m-d'); 
-                        echo "<div class='flex-row flex-center'><h3>".$event_days."</h3>
-                        <input type='checkbox'  name='volunteer_date[]' value='$event_days'"; for($i=0;$i<count($volunteer_date);$i++){foreach($volunteer_date[$i] as $volunteered_date){ if($event_days==$volunteered_date){ echo 'checked';}}}; 
-                        echo"></div>";
-                    }
-   
-                  
+                $startDate = new DateTime($start_date);
+                $interval = new DateInterval('P1D');
+                $realEnd = new DateTime($end_date);
+                $realEnd->add($interval);
+
+
+                $period = new DatePeriod($startDate, $interval, $realEnd);
+                foreach ($period as $date) {
+                    $event_days = $date->format('Y-m-d');
+                    echo "<div class='flex-row flex-center'><h3>" . $event_days . "</h3>
+                        <input type='checkbox'  name='volunteer_date[]' value='$event_days'";
+                    for ($i = 0; $i < count($volunteer_date); $i++) {
+                        foreach ($volunteer_date[$i] as $volunteered_date) {
+                            if ($event_days == $volunteered_date) {
+                                echo 'checked';
+                            }
+                        }
+                    };
+                    echo "></div>";
+                }
+
+
                 ?>
                 <button class="btn btn-solid margin-md" type="submit" id="volunteer-btn" onClick="swithtoUnvolunteer()">Volunteer</button>
             </form>
@@ -480,8 +521,8 @@
             </form>
         </div>
     </div>
-   
- 
+
+
 </body>
 
 

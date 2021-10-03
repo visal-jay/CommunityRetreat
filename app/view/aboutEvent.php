785b746c98ef70f680ce8b697489ac81209b8a84
Visal <visaljayathilaka@gmail.com>
2021-10-03 17:33:12 +0530

    server error

--- app/view/aboutEvent.php
+++ app/view/aboutEvent.php
@@ -425,20 +425,6 @@
                     <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-show-count="false">Tweet</a>
                     <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                 </div>
-                <div class="margin-md" style="margin-bottom: 12px;">
-                    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-                    <div id="fb-root"></div>
-                    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v12.0" nonce="xqUnsUm7"></script>
-
-                    <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
-                </div>
-            </div>
-
-            <div class="flex-row flex-center margin-lg">
-                <div class="margin-md">
-                    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-show-count="false">Tweet</a>
-                    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-                </div>
                 <div class="margin-md" style="margin-bottom: 12px; border-radius:20px; overflow:hidden;width:75px">
                     <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                     <div id="fb-root"></div>

9dd69abad86d722d2e1f9d104ad597ce824a22c2
Visal <visaljayathilaka@gmail.com>
2021-10-06 21:04:23 +0530

    core pagination

--- app/view/organisationGallery.php
+++ app/view/organisationGallery.php
@@ -164,6 +164,18 @@
                 </figure>
             <?php } ?>
         </div>
+        <div>
+            <ul class="pagination">
+                <li><a href="?pageno=1">First</a></li>
+                <li class="<?php if($pageno <= 1){ echo 'disabled'; } ?>">
+                    <a href="<?php if($pageno <= 1){ echo '#'; } else { echo "?pageno=".($pageno - 1); } ?>">Prev</a>
+                </li>
+                <li class="<?php if($pageno >= $total_pages){ echo 'disabled'; } ?>">
+                    <a href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1); } ?>">Next</a>
+                </li>
+                <li><a href="?pageno=<?php echo $total_pages; ?>">Last</a></li>
+            </ul>
+        </div>
     </div>
 </body>
 

b39d67cb0e54717875c357c6a85be4d276b1266d
VenodiW <72806077+VenodiW@users.noreply.github.com>
2021-10-12 22:16:44 +0530

    footer pages

--- app/view/footer.php
+++ app/view/footer.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,52 +10,56 @@
     <script src="https://kit.fontawesome.com/c119b7fc61.js" crossorigin="anonymous"></script>
 
     <style>
-        .main-footer{
+        .main-footer {
             position: relative;
             background-color: #03142d;
-            height:10rem;
+            height: 10rem;
             display: flex;
-            justify-content:center;
+            justify-content: center;
             align-items: center;
             flex-direction: column;
             margin-top: 20px;
             bottom: 0px;
             width: 100%;
-          
-            
+
+
         }
-        .footer-content-container{
+
+        .footer-content-container {
             color: #16c79a;
             width: 50%;
             display: flex;
-            justify-content:space-between;
+            justify-content: space-between;
         }
-        .footer-content{
+
+        .footer-content {
             cursor: pointer;
         }
-        .footer-content:hover{
+
+        .footer-content:hover {
             opacity: 0.3;
         }
-        .feedback-container{
+
+        .feedback-container {
             width: 100%;
-            min-height:100vh;
+            min-height: 100vh;
             position: -webkit-sticky;
             position: fixed;
             z-index: 100;
-            top:0%;
-            left:0%;
+            top: 0%;
+            left: 0%;
             display: flex;
             justify-content: center;
-            display:none;
+            display: none;
             padding-top: 120px;
             backdrop-filter: blur(3px);
         }
- 
-        .unhide
-        {
+
+        .unhide {
             display: flex;
         }
-        .feedback-form{
+
+        .feedback-form {
             width: 30%;
             height: 400px;
             display: flex;
@@ -71,9 +74,10 @@
             position: absolute;
             background-color: white;
         }
-        .feedback-textarea{
 
-            resize:none;
+        .feedback-textarea {
+
+            resize: none;
             padding: 0.3em 0.5em;
             border: 1px solid #ccc;
             font-size: 1rem;
@@ -82,125 +86,145 @@
             font-family: inherit;
             margin-bottom: 0.8rem;
         }
-        .feedback-textarea:focus{
+
+        .feedback-textarea:focus {
             box-shadow: 0px 0px 0px 1px #16c79a;
             border-color: #16c79a;
         }
 
-        .confirm-buttons{
+        .confirm-buttons {
             width: 80%;
             display: flex;
             justify-content: space-evenly;
             flex-direction: row;
         }
-        .overflow{
+
+        .overflow {
             overflow: hidden;
         }
-        .logo img{
+
+        .logo img {
             height: 40px;
             color: #16c79a;
         }
-        #feedback-form{
+
+        #feedback-form {
             width: 100%;
             height: 100%;
         }
 
-        @media screen and (max-width: 1000px){
-            .footer-content-container{
+        @media screen and (max-width: 1000px) {
+            .footer-content-container {
                 width: 60%;
             }
-            .feedback-form{
+
+            .feedback-form {
                 width: 40%;
             }
-            .feedback-form > .confirm-buttons{
+
+            .feedback-form>.confirm-buttons {
                 width: 85%;
             }
         }
-        @media screen and (max-width: 800px){
-            .footer-content-container{
+
+        @media screen and (max-width: 800px) {
+            .footer-content-container {
                 width: 70%;
             }
-            .feedback-form{
+
+            .feedback-form {
                 width: 50%;
             }
         }
-        
-        @media screen and (max-width: 700px){
-            .footer-content-container{
+
+        @media screen and (max-width: 700px) {
+            .footer-content-container {
                 width: 80%;
             }
         }
-        
-        @media screen and (max-width: 600px){
-            
-            footer{
+
+        @media screen and (max-width: 600px) {
+
+            footer {
                 font-size: 13px;
                 height: 6rem;
             }
-            .footer-content-container{
+
+            .footer-content-container {
                 width: 90%;
                 padding: 1rem 0rem 0rem;
-                
+
             }
-            .feedback-form{
+
+            .feedback-form {
                 width: 65%;
             }
-            .feedback-form textarea{
+
+            .feedback-form textarea {
                 width: 18rem;
             }
-            .feedback-form > .confirm-buttons{
+
+            .feedback-form>.confirm-buttons {
                 width: 88%;
             }
 
         }
-        @media screen and (max-width: 420px){
-            footer{
+
+        @media screen and (max-width: 420px) {
+            footer {
                 font-size: 12px;
                 height: 5rem;
             }
-            .footer-content{
+
+            .footer-content {
                 width: 100%
             }
-            .main-footer-content{
+
+            .main-footer-content {
                 padding: 0.1rem;
             }
-            .feedback-form{
-             
+
+            .feedback-form {
+
                 height: 350px;
                 font-size: 13px;
             }
-            .feedback-form textarea{
+
+            .feedback-form textarea {
                 width: 15rem;
             }
-            .feedback-form > .confirm-buttons{
+
+            .feedback-form>.confirm-buttons {
                 width: 95%;
-               
 
-            }
 
-        } 
-        @media screen and (max-width: 340px){
-            footer{
-                font-size: 11px;
-                
-            }
-            .feedback-form{
-             
-                height: 330px;
-                width: 70%;
-                font-size: 12.5px;
-            }
-            .feedback-form textarea{
-                width: 14rem;
-            }
-             .confirm-buttons button{
-                
-                font-size: 0.8rem;
             }
 
         }
 
+        @media screen and (max-width: 340px) {
+            footer {
+                font-size: 11px;
 
+            }
+
+            .feedback-form {
+
+                height: 330px;
+                width: 70%;
+                font-size: 12.5px;
+            }
+
+            .feedback-form textarea {
+                width: 14rem;
+            }
+
+            .confirm-buttons button {
+
+                font-size: 0.8rem;
+            }
+
+        }
     </style>
 </head>
 
@@ -210,17 +234,17 @@
     <div class="feedback-container">
 
         <!-- Feedback form -->
-        <form action="/Systemfeedback/getSystemFeedbacks" method="post" class="feedback-form"> 
-        
-                    <h2>Send us some feedback !</h2>
-                    <p>Do you have a suggestion or found some bug? Let us know in the field below.</p>
-                <textarea cols="40" rows="8" placeholder= "Describe here" class="feedback-textarea form-ctrl"  name="feedback" required></textarea>
-                <div class="confirm-buttons">
-                    
-                        <button   type="submit" class= "btn bg-green clr-white border-green">Submit</button>
-                        <button type="button" onclick="popup()" class= "btn bg-red clr-white border-red">Cancel</button>
-                </div>
-        
+        <form action="/Systemfeedback/getSystemFeedbacks" method="post" class="feedback-form">
+
+            <h2>Send us some feedback !</h2>
+            <p>Do you have a suggestion or found some bug? Let us know in the field below.</p>
+            <textarea cols="40" rows="8" placeholder="Describe here" class="feedback-textarea form-ctrl" name="feedback" required></textarea>
+            <div class="confirm-buttons">
+
+                <button type="submit" class="btn bg-green clr-white border-green">Submit</button>
+                <button type="button" onclick="popup()" class="btn bg-red clr-white border-red">Cancel</button>
+            </div>
+
         </form>
     </div>
 
@@ -234,36 +258,32 @@
                     <p class="footer-content" onclick="popup()">Feedback Us</p>
                 </div>
                 <div class="footer-main-content">
-                    <p class="footer-content">Contact Us</p>
+                    <p onclick="window.location.href='/Main/contactUs'" class="footer-content">Contact Us</p>
                 </div>
                 <div class="footer-main-content">
-                    <p class="footer-content">Terms & conditions</p>
+                    <p onclick="window.location.href='/Main/termsandconditions'" class="footer-content">Terms & Conditions</p>
                 </div>
             </div>
-            <?php }?>
-        <div class= "copyright">
-                <p style="color:gray; margin: 1.5rem 0;"><i class="far fa-copyright"></i>&nbsp2021 Community Retreat, All Right Reserved.</p>
+        <?php } ?>
+        <div class="copyright">
+            <p style="color:gray; margin: 1.5rem 0;"><i class="far fa-copyright"></i>&nbsp2021 Community Retreat, All Right Reserved.</p>
         </div>
         <a class="logo ">
             <img src="/Public/assets/visal logo.png ">
         </a>
-        
-        
+
+
     </footer>
-     
+
 </body>
 
 <script>
-  
-  function popup(){
+    function popup() {
         let feedback_form = document.querySelector(".feedback-container");
-        let body =document.getElementsByTagName("body")[0];
+        let body = document.getElementsByTagName("body")[0];
         body.classList.toggle("overflow");
         feedback_form.classList.toggle("unhide");
-  }
-
-
-
+    }
 </script>
 <script src="/Public/assets/js/input_validation.js"></script>
 
---
+++ app/view/terms&conditions.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Public/assets/newstyles.css">
+    <script src="https://kit.fontawesome.com/c119b7fc61.js" crossorigin="anonymous"></script>
+    <title>Terms & Conditions</title>
+</head>
+
+<style>
+    /* .watermark{
+        display: flex;
+        position: relative; 
+        top: 0; 
+        right: 0;
+        opacity: 0.3;
+    } */
+    /* .background {
+        background-image: url("/Public/assets/visal logo.png");
+        background:blanchedalmond
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover;
+    } */
+
+    .page{
+        min-height: 53%;
+    }
+    .box {
+        max-width: inherit;
+        margin: 0 2rem;
+        text-align: center;
+    }
+</style>
+
+<?php include "nav.php" ?>
+
+<body>
+    <!-- <div class="background"> -->
+    <div class="page">
+        <div class="box">
+            <h3>Terms and Conditions</h3>
+            <p>These terms and conditions outline the rules and regulations for the use of CommunityRetreat. By accessing CommunityRetreat we assume you accept these terms and conditions. </p>
+        </div>
+        <div class="box">
+            <h3>For Organizations</h3>
+            <p>All organizations will be allowed to collect donations and these donations will be credited to a bank account owned by the CommunityRetreat. At the end of the event, donations will be credited to the bank account belongs to the Organization.</p>
+        </div>
+        <div class="box">
+            <h3>For Registered Users</h3>
+            <p>No refunds will be made except for the removal or cancellation of an event after a donation has been made.</p>
+        </div>
+    </div>
+    <!-- <div class="watermark">
+        <img src="/Public/assets/visal logo.png" alt="">
+    </div> -->
+    <?php include "footer.php"; ?>
+</body>
+
+</html>

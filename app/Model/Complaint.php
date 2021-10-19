9d9e132a1b1ed78d40833335baae2e5615ca6fe5
Semini48 <2019cs158@stu.ucsc.lk>
2021-10-20 02:19:16 +0530

    complaint desc

--- app/Model/Complaint.php
+++ app/Model/Complaint.php
@@ -10,7 +10,7 @@ class Complaint extends Model{
     }
 
     public function getComplaints(){
-        $query = 'SELECT reg.username, complaint.complaint_name, complaint.complaint ,complaint.date, complaint.path FROM complaint JOIN registered_user reg ON complaint.complainant_uid = reg.uid';
+        $query = 'SELECT reg.username, complaint.complaint_name, complaint.complaint ,complaint.date, complaint.path FROM complaint JOIN registered_user reg ON complaint.complainant_uid = reg.uid ORDER BY date DESC';
         $result = Model::select($query,[]);                                          
         return $result;
     }

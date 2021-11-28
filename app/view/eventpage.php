c825ae4647ca261ca36f2661be6d4cdcc0fd2925
Manuka Dewanarayana <2019cs031@stu.ucsc.lk>
2021-11-29 02:00:13 +0530

    Title update

--- app/view/eventpage.php
+++ app/view/eventpage.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/Public/assets/newstyles.css">
     <script src="https://kit.fontawesome.com/c119b7fc61.js" crossorigin="anonymous"></script>
-    <title>Event Page</title>
+    <title>CommunityRetreat</title>
 </head>
 <style>
 .photo-container {

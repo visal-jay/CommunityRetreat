c3bfd66f5744f9d3a1cab4f1e433852b7b7de422
Manuka Dewanarayana <2019cs031@stu.ucsc.lk>
2022-03-20 10:18:50 +0530

    Complaint changes

--- app/Model/Complaint.php
+++ app/Model/Complaint.php
@@ -2,7 +2,7 @@
 class Complaint extends Model{
 
     public function addComplaint($data){
-        if($data['status'] == 'event' || $data['status'] == 'organization'){
+        if($data['status'] == 'user' || $data['status'] == 'organization'){
             $user_details = (new User)->getDetails($data['complainant_uid']);
             $query = 'INSERT INTO `complaint` (`complainant_name`, `uid`, `event_id`, `complaint_name`, `complaint`, `status`,`date`,`path`) VALUES (:complainant_name, :uid, :event_id, :complaint_name,:complaint,:status,CURRENT_TIMESTAMP,:path)';
             $params = ["complainant_name" => $user_details['username'], "uid" => $data['uid'], "event_id" => $data['event_id'], "complaint_name" => $data['complaint_name'], "complaint" => $data['complaint'], "status" => $data['status'],"path" => $data['path']];
